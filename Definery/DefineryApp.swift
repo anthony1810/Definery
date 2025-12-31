2622cf4ab56e62fbc3eee883cbcabb0ba9e2fefb
Anthony Tran <qquang269@gmail.com>
2025-12-31 09:59:18 +0700

    Initial Commit

---
+++ Definery/DefineryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DefineryApp.swift
+//  Definery
+//
+//  Created by Anthony on 31/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DefineryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DefineryTests/DefineryTests.swift
@@ -0,0 +1,17 @@
+//
+//  DefineryTests.swift
+//  DefineryTests
+//
+//  Created by Anthony on 31/12/25.
+//
+
+import Testing
+@testable import Definery
+
+struct DefineryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
