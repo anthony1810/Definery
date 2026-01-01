1f8cc0675fefd90eba726258bc52b5f5c53e4f21
Anthony Tran <qquang269@gmail.com>
2026-01-01 14:29:28 +0700

    Add test for cache insertion with words on successful deletion

--- WordCacheTests/Helpers/TestHelpers.swift
+++ WordCacheTests/Helpers/TestHelpers.swift
@@ -5,7 +5,12 @@
 //  Created by Anthony on 1/1/26.
 //
 import Foundation
+import WordFeature
 
 func anyNSError() -> NSError {
     NSError(domain: "any", code: 0)
+}
+
+func uniqueWord() -> Word {
+    Word(id: UUID(), text: "word-\(UUID())", language: "en", phonetic: nil, meanings: [])
 }
