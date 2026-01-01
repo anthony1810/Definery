908aeab0212f3214f67aa1e04dc2ac46c2f5287d
Anthony Tran <qquang269@gmail.com>
2026-01-01 14:29:28 +0700

    Add WordCache framework and WordCacheTests target

---
+++ WordCacheTests/WordCacheTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordCacheTests.swift
+//  WordCacheTests
+//
+//  Created by Anthony on 1/1/26.
+//
+
+import Testing
+@testable import WordCache
+
+struct WordCacheTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
