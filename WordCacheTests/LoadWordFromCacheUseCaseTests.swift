62ba87446a13cfe21d8699e7f48ec171afd1fe4a
Anthony Tran <qquang269@gmail.com>
2026-01-01 14:29:29 +0700

    Add LoadWordFromCacheUseCaseTests skeleton
    - Add test stubs for load use case (all commented for TDD)
    - Move WordCacheProtocol and LocalWordLoader to TestHelpers
    - No cache expiration - cache forever strategy

---
+++ WordCacheTests/LoadWordFromCacheUseCaseTests.swift
@@ -0,0 +1,30 @@
+//
+//  LoadWordFromCacheUseCaseTests.swift
+//  WordCacheTests
+//
+//  Created by Anthony on 1/1/26.
+//
+
+import Foundation
+import Testing
+import WordFeature
+
+@testable import WordCache
+
+struct LoadWordFromCacheUseCaseTests {
+
+//    @Test func load_requestsCacheRetrieval() async {}
+
+//    @Test func load_failsOnRetrievalError() async {}
+
+//    @Test func load_deliversNoWordsOnEmptyCache() async {}
+
+//    @Test func load_deliversCachedWords() async {}
+
+//    @Test func load_hasNoSideEffectOnRetrievalError() async {}
+
+//    @Test func load_hasNoSideEffectOnEmptyCache() async {}
+
+//    @Test func load_hasNoSideEffectOnNonEmptyCache() async {}
+
+}
