ba3567cde6486de4c4dd19c2bdcdeba58a4072cc
Anthony Tran <qquang269@gmail.com>
2026-01-02 08:58:47 +0700

    Add SwiftDataWordStore with first retrieve test
    - Create SwiftDataWordStore implementing WordStorageProtocol
    - Add retrieve_deliversEmptyOnEmptyCache test
    - Add test helpers (MemoryLeakTracker, TestHelpers)
    - Configure WordCacheInfrastructure test plan

---
+++ WordCacheInfrastructure/SwiftDataWordStore.swift
@@ -0,0 +1,25 @@
+//
+//  SwiftDataWordStore.swift
+//  Definery
+//
+//  Created by Anthony on 2/1/26.
+//
+import Foundation
+import WordCache
+
+public final class SwiftDataWordStore: WordStorageProtocol {
+    public init() {}
+    
+    public func deleteCachedWords() async throws {
+        
+    }
+    
+    public func insertCache(words: [LocalWord]) async throws {
+        
+    }
+    
+    public func retrieveWords() async throws -> [LocalWord] {
+        []
+    }
+    
+}
