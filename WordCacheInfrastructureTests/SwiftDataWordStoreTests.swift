3600576e4297f9a6e8c6c95835553553e275c13f
Anthony Tran <qquang269@gmail.com>
2026-01-02 08:58:47 +0700

    Add SwiftDataWordStore with first retrieve test
    - Create SwiftDataWordStore implementing WordStorageProtocol
    - Add retrieve_deliversEmptyOnEmptyCache test
    - Add test helpers (MemoryLeakTracker, TestHelpers)
    - Configure WordCacheInfrastructure test plan

---
+++ WordCacheInfrastructureTests/SwiftDataWordStoreTests.swift
@@ -0,0 +1,51 @@
+//
+//  SwiftDataWordStoreTests.swift
+//  Definery
+//
+//  Created by Anthony on 2/1/26.
+//
+
+import Testing
+import WordCacheInfrastructure
+
+final class SwiftDataWordStoreTests {
+    private var sutTracker: MemoryLeakTracker<SwiftDataWordStore>?
+    
+    deinit {
+        sutTracker?.verify()
+    }
+    
+    @Test func retrieve_deliversEmptyOnEmptyCache() async throws {
+        let sut = try makeSUT()
+        
+        let words = try await sut.retrieveWords()
+        
+        #expect(words.isEmpty)
+    }
+    
+}
+// MARK: - Helpers
+
+extension SwiftDataWordStoreTests {
+    private func makeSUT(
+        fileId: String = #fileID,
+        filePath: String = #filePath,
+        line: Int = #line,
+        column: Int = #column
+    ) throws -> SwiftDataWordStore {
+        let sut = SwiftDataWordStore()
+        
+        sutTracker = MemoryLeakTracker(
+            instance: sut,
+            sourceLocation: SourceLocation(
+                fileID: fileId,
+                filePath: filePath,
+                line: line,
+                column: column
+            )
+        )
+
+        return sut
+    }
+}
+
