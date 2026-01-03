06c156ff6beac18ae2115d6b9a71c06d47535ba8
Anthony Tran <qquang269@gmail.com>
2026-01-03 21:51:25 +0700

    Wire composition root with HomeUIComposer
    - Add HomeUIComposer to compose HomeView with dependencies
    - Remove placeholder ContentView
    - Wire DefineryApp with WordLoaderFactory for shared dependencies
    - Extract WordLoaderFactory typealias to module level
    - Add WordLoaderProtocol conformance to LocalWordLoader

---
+++ Definery/Composer/HomeUIComposer.swift
@@ -0,0 +1,20 @@
+//
+//  HomeUIComposer.swift
+//  Definery
+//
+//  Created by Anthony on 3/1/26.
+//
+
+import SwiftUI
+import WordFeature
+
+enum HomeUIComposer {
+    static func homeComposedWith(
+        loader: @escaping WordLoaderFactory
+    ) -> HomeView {
+        let viewState = HomeViewState()
+        let viewStore = HomeViewStore(loader: loader)
+        
+        return HomeView(viewStore: viewStore, viewState: viewState)
+    }
+}
